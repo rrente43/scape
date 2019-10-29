331f3080b5a4851564ca7c7cd8221b61b4e14188
Rubi Renteria <rubirenteria43@gmail.com>
2019-10-28 23:02:47 -0600

    and more routes

--- config/routes.js
+++ config/routes.js
@@ -1,3 +1,7 @@
+var scrape = require("../scripts/scrape");
+var headlinesController = require("../controllers/headline.js");
+var notesController = require("../controllers/notes");
+
 module.exports = function(router){
     router.get("/", function(req,res){
         res.render("home");
@@ -5,5 +9,50 @@ module.exports = function(router){
 
     router.get("/saved", function(req,res){
         res.render("saved");
+    });
+
+    router.get("/api/fetch", function(req,res){
+        headlinesController.fetch(function(err,docs){
+            if(!docs || docs.insertedCount === 0){
+                res.json({
+                    message: "No new articles today, come back tomorrow"
+                });
+            }
+            else{
+                res.json({
+                    message: "added" + docs.insertedCount + "new articles"
+                });
+            }
+        });
+    });
+    router.get("/api/headlines", function(req,res){
+        var query ={};
+        if(req.query.saved){
+            query = req.query;
+        }
+        headlinesController.get(query, function(data){
+            res.json(data);
+        });
+    });
+    router.delete("/api/headlines/:id", function(req,res){
+        var query = {};
+        query._id = req.params.id;
+        headlinesController.delete(query, function(err,data){
+            res.json(data);
+        });
+    });
+    router.patch("/api/headlines", function(req,res){
+        headlinesController.update(req.body,function(err,data){
+            res.json(data);
+        });
+    });
+    router.get("/api/notes/:headline_id?", function(req,res){
+        var query ={};
+        if (req.params.headline_id){
+            query._id = req.params.headline_id;
+        }
+        notesController.get(query,function(err,data){
+            res.json(data);
+        });
     });
 }
